29d5465df9fc91afc5f96acb2d1a845f1e45075e
inSideos-designs <spatade3@gatech.edu>
2025-11-24 20:35:47 -0500

    Improve steam animation and fix Render deployment

--- App.tsx
+++ App.tsx
@@ -12,9 +12,15 @@ const App: React.FC = () => {
   return (
     <div className="font-sans text-gray-800 bg-premium-dark min-h-screen relative overflow-x-hidden">
       {/* Global Steam Background */}
-      <div className="fixed inset-0 pointer-events-none z-0 opacity-20">
-        <div className="absolute top-0 left-1/4 w-96 h-96 bg-brand-cyan rounded-full mix-blend-screen filter blur-[128px] animate-float"></div>
-        <div className="absolute bottom-0 right-1/4 w-96 h-96 bg-brand-blue rounded-full mix-blend-screen filter blur-[128px] animate-float" style={{ animationDelay: '2s' }}></div>
+      {/* Global Steam Background */}
+      <div className="steam-container">
+        <div className="fog-container">
+          <div className="fog-img"></div>
+          <div className="fog-img-2"></div>
+        </div>
+        <div className="steam-vent" style={{ left: '20%', animationDelay: '0s' }}></div>
+        <div className="steam-vent" style={{ left: '50%', animationDelay: '2s' }}></div>
+        <div className="steam-vent" style={{ left: '80%', animationDelay: '1s' }}></div>
       </div>
 
       <div className="relative z-10">
